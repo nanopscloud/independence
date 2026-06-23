50a975a4e37a3f0c34198334d982362aa752ccff
Sami Salminen <48570580+nanopscloud@users.noreply.github.com>
2026-06-23 13:09:28 +0300

    Refactor hello.sh to remove empty echo statements

--- hello.sh
+++ hello.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 # hello.sh — simple hello world script
 
-echo " "
 echo "👋 Hello from nanopscloud/independence!"
 
 kuori=$BASH
 kuoriversio=$BASH_VERSION
 
-echo "Script executed successfully on $(uname -s) ($(uname -m)) running in $kuori version $kuoriversio"
+echo "Script executed successfully on $(uname -s) ($(uname -m)) in $kuori version $kuoriversio"
 echo "$(date)"
-echo " "
