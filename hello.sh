3a79764dad9e68c62283def3050f91b42b1b25b2
Sami Salminen <48570580+nanopscloud@users.noreply.github.com>
2026-06-23 12:55:23 +0300

    Update hello.sh to use BASH_VERSION variable

--- hello.sh
+++ hello.sh
@@ -2,7 +2,7 @@
 # hello.sh — simple hello world script
 
 echo "👋 Hello from nanopscloud/independence!"
-kuori=$SHELL
-echo "Script executed successfully on $(uname -s) ($(uname -m)) in $kuori"
+kuori=$BASH_VERSION
+echo "Script executed successfully on $(uname -s) ($(uname -m)) running in $kuori"
 echo "$(date)"
 echo " "
